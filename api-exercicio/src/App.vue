7f3530c065ef56e76e998608b7dcaccddb4c4e86
HericMD <hericmdms@gmail.com>
2022-09-20 09:22:37 -0300

    Heric

---
+++ api-exercicio/src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      resultado: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+      const { data } = await axios.get(url);
+      this.endereco = data;
+    },
+  },
+};
+</script>
+<template>
+  <h1>Buscar CEP</h1>
+  <p>Digite o cep</p>
+  <input type="text" v-model="cep" placeholder="xxxxxxxx" />
+  <button @click="buscar">Buscar</button>
+  <form>
+    <label for="rua">Rua</label>
+    <input name="rua" type="text" v-model="endereco.logradouro" />
+  </form>
+</template>
+<style></style>
